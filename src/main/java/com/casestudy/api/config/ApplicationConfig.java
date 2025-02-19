c75ad831dfc15e536a24ebe688d8ef5fff8337cc
Dusan <dusan@me.com>
2025-02-19 09:16:21 +0100

    update

--- src/main/java/com/casestudy/api/config/ApplicationConfig.java
+++ src/main/java/com/casestudy/api/config/ApplicationConfig.java
@@ -2,12 +2,14 @@ package com.casestudy.api.config;
 
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableAsync
+@EnableRetry
 @EnableTransactionManagement
 public class ApplicationConfig implements AsyncConfigurer {
     @Override
--- src/main/java/com/casestudy/api/service/impl/DefaultNotifyService.java
+++ src/main/java/com/casestudy/api/service/impl/DefaultNotifyService.java
@@ -1,14 +1,21 @@
 package com.casestudy.api.service.impl;
 
+import com.casestudy.api.config.CustomAsyncExceptionHandler;
+import com.casestudy.api.exception.NotifyServiceTimeoutException;
+import com.casestudy.api.exception.NotifyServiceUnreachableException;
 import com.casestudy.api.model.Ordered;
 import com.casestudy.api.rest.RestTemplateResponseErrorHandler;
 import com.casestudy.api.service.NotifyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,6 +27,8 @@ public class DefaultNotifyService implements NotifyService {
 
     private final RestTemplate restTemplate;
 
+    Logger logger = LoggerFactory.getLogger(DefaultNotifyService.class);
+
     @Autowired
     public DefaultNotifyService(RestTemplateBuilder restTemplateBuilder) {
         restTemplate = restTemplateBuilder
@@ -28,11 +37,18 @@ public class DefaultNotifyService implements NotifyService {
     }
 
     @Override
+    @Retryable(retryFor = {NotifyServiceUnreachableException.class, NotifyServiceTimeoutException.class})
     public ResponseEntity<String> notify(Ordered ordered) {
         HttpEntity<Ordered> request = new HttpEntity<>(ordered);
 
         String url = environment.getProperty("notify-service.url", "http://localhost:8000/notify");
 
         return restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+    }
+
+    @Recover
+    public ResponseEntity<String> notifyFailed(Ordered ordered) {
+        logger.error("Notify failed");
+        return ResponseEntity.unprocessableEntity().build();
     }
 }
