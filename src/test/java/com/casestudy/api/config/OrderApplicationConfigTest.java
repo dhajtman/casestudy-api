467d1963566769117922610f1d7ba073d4c0f9f5
Dusan <dusan@me.com>
2025-03-10 15:34:41 +0100

    app config test

---
+++ src/test/java/com/casestudy/api/config/OrderApplicationConfigTest.java
@@ -0,0 +1,38 @@
+package com.casestudy.api.config;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class OrderApplicationConfigTest {
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    public void taskSchedulerBeanPresence() {
+        Object o1 = context.getBean(TaskScheduler.class);
+        Object o2 = context.getBean("taskScheduler");
+
+        Assertions.assertNotNull(o1);
+        Assertions.assertNotNull(o2);
+        Assertions.assertInstanceOf(ThreadPoolTaskScheduler.class, o1);
+        Assertions.assertInstanceOf(ThreadPoolTaskScheduler.class, o2);
+    }
+
+    @Test
+    public void asyncExecutorBeanPresence() {
+        Object o1 = context.getBean(ThreadPoolTaskExecutor.class);
+        Object o2 = context.getBean("asyncExecutor");
+
+        Assertions.assertNotNull(o1);
+        Assertions.assertNotNull(o2);
+        Assertions.assertInstanceOf(ThreadPoolTaskExecutor.class, o1);
+        Assertions.assertInstanceOf(ThreadPoolTaskExecutor.class, o2);
+    }
+}
